430e0954627af3db61ea7d631e7cc5cd79752729
Arun Kumar R <aruntutter19@gmail.com>
2023-08-06 14:58:40 +0530

    structure ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
+// inputs
+const currencyOneEl = document.getElementById('currency-one');
+const currencyTwoEl = document.getElementById('currency-two');
+const amountOneEl = document.getElementById("amount-one");
+const amountTwoEl = document.getElementById("amount-two");
+
+const btnSwap = document.getElementById('swap');
+const rateEl = document.getElementById('rate');
+
 // global variables
+const url = `http://api.exchangeratesapi.io/v1/latest?access_key=f87a35900308f926d8cbddbd2a5578f2&format=1`;
 
 // functions
 function init() {}
