be1e8c4e845e6017c551f57858d54a082eb2157c
Arun Kumar R <aruntutter19@gmail.com>
2023-08-06 18:55:33 +0530

    final code

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,66 @@
 "use strict";
 
 // inputs
-const currencyOneEl = document.getElementById('currency-one');
-const currencyTwoEl = document.getElementById('currency-two');
+const currencyOneEl = document.getElementById("currency-one");
+const currencyTwoEl = document.getElementById("currency-two");
 const amountOneEl = document.getElementById("amount-one");
 const amountTwoEl = document.getElementById("amount-two");
 
-const btnSwap = document.getElementById('swap');
-const rateEl = document.getElementById('rate');
+const btnSwap = document.getElementById("swap");
+const rateEl = document.getElementById("rate");
 
 // global variables
-const url = `http://api.exchangeratesapi.io/v1/latest?access_key=f87a35900308f926d8cbddbd2a5578f2&format=1`;
+const url = ` https://v6.exchangerate-api.com/v6/972fc1c6613911a718ec109f/latest/`;
 
 // functions
-function init() {}
+function init() {
+  getCurrencyNames();
+  getExchangeRates();
+}
+
+async function getCurrencyNames() {
+  const response = await fetch(`${url}INR`);
+  const data = await response.json();
+  const countriesArr = Object.keys(data.conversion_rates);
+  addCurrencyCodes(currencyOneEl, countriesArr);
+  addCurrencyCodes(currencyTwoEl, countriesArr);
+}
+
+function addCurrencyCodes(ele, currencyArr) {
+  currencyArr.forEach((countryCode) => {
+    // create element
+    const optionEl = document.createElement("option");
+    optionEl.value = countryCode;
+    optionEl.innerText = countryCode;
+    if (countryCode !== "INR") {
+      ele.appendChild(optionEl);
+    }
+  });
+}
+
+async function getExchangeRates() {
+  const currencyOne = currencyOneEl.value;
+  const currencyTwo = currencyTwoEl.value;
+
+  const response = await fetch(`${url}${currencyOne}`);
+  const data = await response.json();
+
+  const rate = data.conversion_rates[currencyTwo];
+  rateEl.innerText = `1 ${currencyOne} = ${rate.toFixed(2)} ${currencyTwo}`;
+  amountTwoEl.value = (amountOneEl.value * rate).toFixed(2);
+}
 
 //event listeners
+currencyOneEl.addEventListener("change", getExchangeRates);
+currencyTwoEl.addEventListener("change", getExchangeRates);
+
+amountOneEl.addEventListener("input", getExchangeRates);
+
+btnSwap.addEventListener('click', ()=>{
+  [currencyOneEl.value, currencyTwoEl.value] = [currencyTwoEl.value, currencyOneEl.value];
+
+  getExchangeRates();
+})
 
 // initial settings
 init();
